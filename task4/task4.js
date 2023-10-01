33c1d0f091ec2ce495cd2ef866c3d9bff5b9262b
mithunmr888 <mithunmr888@gmail.com>
2023-10-01 12:23:32 +0530

    done with queryselectorall

--- task4/task4.js
+++ task4/task4.js
@@ -1,6 +1,6 @@
 //queryselector
 
-var header = document.querySelector('#main-header');
+/*var header = document.querySelector('#main-header');
 console.log(header);
 header.style.borderBottom = 'solid 4px #ccc';
 
@@ -19,13 +19,43 @@ lastitem.style.color = 'blue';
 var seconditem = document.querySelector('.list-group-item:nth-child(2)');
 seconditem.style.color = 'coral';
 
-/*Deliverable
+Deliverable
 Make the 2nd item have green background color
-Make the 3rd item invisible*/
+Make the 3rd item invisible
 
 seconditem.style.background = 'green';
 
 var thirditem = document.querySelector('.list-group-item:nth-child(3)');
-thirditem.textContent = '';
+thirditem.textContent = '';*/
+
+
+
+//query selector all
+
+/*var titles = document.querySelectorAll('.title');//here . means class
+console.log(titles);
+
+titles[0].textContent = 'Hello';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+
+for(var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
+}*/
+
+
+/*Deliverable
+Using QuerySelectorALL change the font color to green for 2nd item in the item list
+Choose all the odd elements and make their background green using QuerySelectorALL﻿*/
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+for(var i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = '#00ff00';
+    even[0].style.color = 'green';
+}
+
 
 
