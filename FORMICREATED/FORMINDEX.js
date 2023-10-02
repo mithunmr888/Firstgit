318a1a9944604e0bcd4a8fe377773efcc4a4671e
mithunmr888 <mithunmr888@gmail.com>
2023-10-02 12:32:09 +0530

    Booking appointment App done

---
+++ FORMICREATED/FORMINDEX.js
@@ -0,0 +1,26 @@
+function onsignup(event) {
+    event.preventDefault(); // Prevents the form from submitting the traditional way
+
+    // Get the form values
+    var fname = document.getElementById('fname').value;
+    var email = document.getElementById('email').value;
+    var pword = document.getElementById('pword').value;
+    var dword = document.getElementById('dword').value;
+
+    // Create an object to hold the user details
+    var userDetails = {
+        fname: fname,
+        email: email,
+        pword: pword,
+        dword: dword
+    };
+
+    // Convert the object to a JSON string
+    var userDetailsJSON = JSON.stringify(userDetails);
+
+    // Store the JSON string in local storage
+    localStorage.setItem('userDetails', userDetailsJSON);
+
+    // Optional: Provide some feedback to the user
+    alert('Details stored successfully!');
+}
